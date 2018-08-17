e81e0ebcaa1e632f3f00b282eda25a06092067f1
villeimm <ville.immonen2@gmail.com>
2018-08-17 15:57:28 +0300

    e

---
+++ Blop/Models/BlobUser.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blop.Models
+{
+    public class BlobUser
+    {
+        public string ID { get; set; }
+        public string PictureUrl { get; set; }
+    }
+}
---
+++ Blop/Models/StorageUser.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blop.Models
+{
+    public class StorageUser
+    {
+        [DisplayName("Etunimi")]
+        public string FirstName { get; set; }
+        [DisplayName("Sukunimi")]
+        public string LastName { get; set; }
+        [DisplayName("Sukupuoli")]
+        public string Gender { get; set; }
+        [DisplayName("Ikä")]
+        public int Age { get; set; }
+        public string PictureUrl { get; set; }
+        public string ThumbUrl { get; set; }
+    }
+}
