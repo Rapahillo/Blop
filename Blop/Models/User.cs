7fb570bc35055775c04e0cee963e656658459b1d
villeimm <ville.immonen2@gmail.com>
2018-08-17 14:34:59 +0300

    b

---
+++ Blop/Models/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blop.Models
+{
+    public class User
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Gender { get; set; }
+        public int Age { get; set; }
+        public string PictureUrl { get; set; }
+    }
+}
