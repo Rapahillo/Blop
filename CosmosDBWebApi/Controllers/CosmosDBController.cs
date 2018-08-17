4bddbe63bf5417df2b54193a2a8a0a97377f6385
SariPaavilainen <sari.paavilainen@iki.fi>
2018-08-17 15:30:09 +0300

    CosmosDBWebApin koodia lisää

---
+++ CosmosDBWebApi/Controllers/CosmosDBController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace CosmosDBWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CosmosDBController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+        private readonly DocumentClient _client;
+        private const string _dbName = "BlopDB";
+        private const string _collectionName = "BlopCollection";
+
+        public CosmosDBController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            var endpointUri = _configuration[];
+            var key = _configuration[];
+            
+        }
+
+    }
+}
---
+++ CosmosDBWebApi/Controllers/DocumentClient.cs
@@ -0,0 +1,6 @@
+﻿namespace CosmosDBWebApi.Controllers
+{
+    internal class DocumentClient
+    {
+    }
+}
